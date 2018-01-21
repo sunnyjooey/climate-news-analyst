560b7a799e1bf331cd0eb86d3731210ae1ba4484
Victor Manuel Hermilo Vilchis Tella <vmhvilchis@gmail.com>
2018-01-21 16:04:39 -0600

    Incrementing test limit to 150 news

--- run_scraper.py
+++ run_scraper.py
@@ -8,6 +8,6 @@ filename = 'news_USA.pkl'
 filepath=os.path.join(folder,filename)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 #(language='English', year='2017', country='USA', sourcetype='Newspaper',  searchterm='climate%20change', limit=None)
-thing = ns.get_all_news(country='USA',limit=50)
+thing = ns.get_all_news(country='USA',limit=150)
 with open(filepath, 'wb') as output:
     pickle.dump(thing, output)

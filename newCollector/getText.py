1631e515652560d87b9a8be8829db1e037b864f0
sunnyjooey <sunnyjooey@gmail.com>
2018-02-18 17:30:46 -0600

    better regex for chopping off unneeded text at the end

--- newCollector/getText.py
+++ newCollector/getText.py
@@ -1,18 +1,7 @@
-import os
-import pandas
-from bs4 import BeautifulSoup
-from bs4.element import NavigableString
-import re
-import urllib
-import math
-
-
 def extract_news(html, country):
     
-    article_dict = {'country': country}
-    soup = BeautifulSoup(html, 'lxml')
-    #testing 
-    
+    article_dict = {'filename': html.name, 'country': country}
+    soup = BeautifulSoup(html, 'lxml')  
     
     # title
     title0 = soup.find_all('div', {'class' : 'title'})
@@ -20,16 +9,23 @@ def extract_news(html, country):
     title = re.sub(r'\<.*\>', '', title1)
     article_dict['title'] = title
 
+    
     # text
     body = soup.find_all('div', {'class' : 'body'})
-    text0 = "".join([t for t in body[0].contents if type(t)==NavigableString]) #get text only from content, not children
-    text1 = re.sub(r"Source- .+","", text0) #cleaning
-    text2 = re.sub(r"© .+","", text1) #cleaning
-    text3 = re.sub(r"(STORY CAN END HERE)","", text2) #cleaning 
-    text4 = re.sub(r"(EDITORS: STORY CAN END HERE)","", text3) #cleaning
-    text5 = re.sub(r"(EDITORS: )","", text4) #cleaning
-    text = re.sub(r"^([A-Z]{2,}\s?[A-Z]{2,})", "", text5) #take out first word if all caps (it is a location)
+    text0 = body[0].text
+    
+    # take out first word if all caps (it is a location)
+    text1 = re.sub(r"^([A-Z]{2,}\s?[A-Z]{2,})", "", text0) 
+    # take out from end
+    text2 = re.sub(r"\S*@\S*\s?", "ZXCVB", text1) #email
+    text3 = re.sub(r"ZXCVB(.*)|Published by HT (.*)|Caption (.*)|Copyright\s\((.*)|Copyright: (.*)|Source- (.*)|© (.*)|Digital Content Services (.*)|Syndication with permission (.*)|www(.*)|http(.*)", "", text2)
+    # take out from middle 
+    text4 = re.sub(r"(STORY CAN END HERE)","", text3) #cleaning 
+    text5 = re.sub(r"(EDITORS: STORY CAN END HERE)","", text4) #cleaning
+    text = re.sub(r"(EDITORS: )","", text5) #cleaning
+
     article_dict['text'] = text
+    
     
     # source & date
     source = soup.find_all('div', {'class' : 'source'})
@@ -39,11 +35,13 @@ def extract_news(html, country):
     newssource = pat2.findall(source[0].text)
     article_dict['source'] = newssource[0] 
 
+    
     # author  
     author0 = soup.find_all('span', {'class' : 'val'})
     author = re.sub(r'\<.*\>', '', author0[0].text) #Take out <>
     article_dict['author'] = author
 
+    
     # section
     section0 = soup.find_all('span', {'class' : 'lbl'}, string="Section: ")
     try: 
@@ -61,7 +59,7 @@ def getText():
     for root, dirs, filenames in os.walk(indir):
         for f in filenames:
             filepath = os.path.join(root,f)
-            with open(filepath, 'r', encoding='latin1') as testfile:
+            with open(filepath, 'r', encoding='utf-8') as testfile:
                 try:
                     articleDict = extract_news(testfile, f[:2])
                     articleList.append(articleDict)
@@ -70,6 +68,5 @@ def getText():
                     raise
     return pandas.DataFrame(articleList)
 
-
 df = getText()
-df.to_csv('news_df.csv')
+#df.to_csv('news_df.csv')
